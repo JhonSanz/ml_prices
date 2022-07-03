68fb48917b66dda0c2bf41433de1ffbcb61d16ed
JhonSanz <ingjhonsanz@utp.edu.co>
2022-07-03 17:37:20 -0500

    Added neural network

--- linear_regression.py
+++ linear_regression.py
@@ -1,49 +1,66 @@
 import pandas as pd
-import pandas_datareader as web
-from sklearn.linear_model import LinearRegression
-from sklearn.model_selection import train_test_split
-from sklearn.metrics import mean_squared_error, r2_score, mean_absolute_error
 import pandas_ta as ta
-import matplotlib.pyplot as plt
-
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+from sklearn.metrics import classification_report, confusion_matrix
+from sklearn.neural_network import MLPClassifier
+import joblib
 
 SHOW_PLOT = False
-EMA_LENGTH = 500
-EMA_IND = f'EMA_{EMA_LENGTH}'
+SLOW = 500
+FAST = 1
+AO = f'AO_{FAST}_{SLOW}'
 
-df = web.DataReader(
-    '^GSPC', data_source='yahoo', start='2012-01-01', end='2022-06-28'
-)
-df = df[["Close"]]
-df.ta.ema(close="Close", length=EMA_LENGTH, append=True)
-df = df.iloc[EMA_LENGTH:, :]
+def get_data(file_name, train_mode):
+    if train_mode:
+        df = pd.read_csv(file_name)
+        df.set_index(pd.DatetimeIndex(df["date"]), inplace=True)
+        df = df[["close", "high", "low", "type"]]
+    else:
+        df = pd.read_csv(file_name, sep="\t")
+        df["<DATE>"] = df["<DATE>"] + " " + df["<TIME>"]
+        df.rename(columns={
+            "<DATE>": "date",
+            "<OPEN>": "open",
+            "<HIGH>": "high",
+            "<LOW>": "low",
+            "<CLOSE>": "close",
+        }, inplace=True)
+        df["date"] = pd.to_datetime(df["date"], format="%Y.%m.%d %H:%M")
+        df.set_index(pd.DatetimeIndex(df["date"]), inplace=True)
+        df = df[["high", "low", "close"]]
+    df.ta.ao(
+        high="high", low="low", slow=SLOW,
+        fast=1, append=True
+    )
+    df = df.iloc[SLOW:, :]
+    return df
 
-X_train, X_test, y_train, y_test = train_test_split(
-    df[[EMA_IND]], df[["Close"]], test_size=.2
-)
-model = LinearRegression()
-model.fit(X_train, y_train)
-y_pred = model.predict(X_test)
+try:
+    model = joblib.load("model.pkl")
+except FileNotFoundError:
+    df = get_data("us100_labeled.csv", True)
+    scaler = StandardScaler()
+    X_train, X_test, y_train, y_test = train_test_split(
+        (df[[AO, "close"]]), df[["type"]], test_size=0.2
+    )
+    scaler.fit(X_train)
+    X_train = scaler.transform(X_train)
+    X_test = scaler.transform(X_test)
 
+    model = MLPClassifier(
+        hidden_layer_sizes=(10, 8, 10),
+        activation='relu',
+        solver='adam',
+        max_iter=1000
+    )
+    model.fit(X_train, y_train.values.ravel())
+    predictions = model.predict(X_test)
+    print(confusion_matrix(y_test, predictions))
+    print(classification_report(y_test, predictions))
+    joblib.dump(model, "model.pkl") 
 
-# another test :D
-df = web.DataReader(
-    '^GSPC', data_source='yahoo', start='2012-01-01', end='2022-06-29'
-)
-df = df[["Close"]]
-df.ta.ema(close="Close", length=EMA_LENGTH, append=True)
-last_60_days = df[-500:]
-print(last_60_days)
-y_pred = model.predict([[4038.759228]])
-print(y_pred)
-
-if SHOW_PLOT:
-    plt.style.use('fivethirtyeight')
-    plt.figure(figsize=(16,8))
-    plt.title('Model')
-    plt.xlabel('Date',fontsize=18)
-    plt.ylabel('Close Price USD($)',fontsize=18)
-    plt.plot(df[EMA_IND])
-    plt.plot(df['Close'])
-    plt.legend(['Train','Val','Predictions'],loc='lower right')
-    plt.show()
+df = get_data("us100_30M_07.csv", False)
+print(df[305:])
+df = df[[AO, "close"]]
+print(model.predict([[330.26310, 11920.67]]))
