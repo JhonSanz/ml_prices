d18e6a92ae47ef4428c05db7a481a4fc9f861e05
JhonSanz <ingjhonsanz@utp.edu.co>
2022-07-06 16:46:30 -0500

    Trying to verify the overfitting.

--- linear_regression.py
+++ linear_regression.py
@@ -2,7 +2,7 @@ import pandas as pd
 import pandas_ta as ta
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
-from sklearn.metrics import classification_report, confusion_matrix
+from sklearn.metrics import classification_report, confusion_matrix, mean_squared_error
 from sklearn.neural_network import MLPClassifier
 import joblib
 
@@ -42,13 +42,41 @@ class Solver:
         df = df.iloc[self.SLOW:, :]
         return df
 
-    def get_model(self):
+    def define_sets(self):
         df = self.get_data("us100_labeled.csv", True)
-        self.scaler = StandardScaler()
+        df["bad_col"] = -100
         X_train, X_test, y_train, y_test = train_test_split(
-            (df[[self.AO, "close", "SMA_500", "RSI_14"]]
-                ), df[["type"]], test_size=0.2
+            # df[[self.AO, "close", "SMA_500", "RSI_14"]],
+            df[["bad_col"]],
+            df[["type"]], test_size=0.2
         )
+        X_train.to_csv("train_data/X_train_bad.csv")
+        X_test.to_csv("train_data/X_test_bad.csv")
+        y_train.to_csv("train_data/y_train_bad.csv")
+        y_test.to_csv("train_data/y_test_bad.csv")
+
+    def get_train_data(self):
+        X_train = pd.read_csv("train_data/X_train_bad.csv")
+        X_test = pd.read_csv("train_data/X_test_bad.csv")
+        y_train = pd.read_csv("train_data/y_train_bad.csv")
+        y_test = pd.read_csv("train_data/y_test_bad.csv")
+        X_train.set_index(pd.DatetimeIndex(X_train["date"]), inplace=True)
+        X_test.set_index(pd.DatetimeIndex(X_test["date"]), inplace=True)
+        y_train.set_index(pd.DatetimeIndex(y_train["date"]), inplace=True)
+        y_test.set_index(pd.DatetimeIndex(y_test["date"]), inplace=True)
+        X_train = X_train.iloc[:, 1:]
+        X_test = X_test.iloc[:, 1:]
+        y_train = y_train.iloc[:, 1:]
+        y_test = y_test.iloc[:, 1:]
+        return X_train, X_test, y_train, y_test
+
+    def get_model(self):
+        try:
+            X_train, X_test, y_train, y_test = self.get_train_data()
+        except:
+            self.define_sets()
+            X_train, X_test, y_train, y_test = self.get_train_data()
+        self.scaler = StandardScaler()
         self.scaler.fit(X_train)
         X_train = self.scaler.transform(X_train)
         X_test = self.scaler.transform(X_test)
@@ -57,28 +85,34 @@ class Solver:
             model = joblib.load("model.pkl")
         except FileNotFoundError:
             model = MLPClassifier(
-                hidden_layer_sizes=(30, 30, 30),
+                hidden_layer_sizes=(100, 100, 100),
                 activation='relu',
                 solver='adam',
-                max_iter=1000
+                max_iter=10000
             )
             model.fit(X_train, y_train.values.ravel())
+            # joblib.dump(model, "model.pkl")
 
-        predictions = model.predict(X_test)
-        print(confusion_matrix(y_test, predictions))
-        print(classification_report(y_test, predictions))
-        joblib.dump(model, "model.pkl")
+        predictions_train = model.predict(X_train)
+        print("model: 100, 100, 100")
+        print("train: ", mean_squared_error(y_train[["type"]], predictions_train))
+        print(classification_report(y_train, predictions_train))
+
+        predictions_test = model.predict(X_test)
+        print("test: ", mean_squared_error(y_test[["type"]], predictions_test))
+        print(classification_report(y_test, predictions_test))
         return model
 
     def calculate(self):
         model = self.get_model()
-        df = self.get_data("us100_30M.csv", False)
-        # df = self.get_data("US100Cash_M30_2020_01_01_2020_12_31.csv", False)
-        df = df[[self.AO, "close", "SMA_500", "RSI_14"]]
-        train_data = self.scaler.transform(df)
-        prediction = model.predict(train_data)
-        df["result"] = prediction
-        df.to_csv("results.csv")
+        
+        # df = self.get_data("us100_30M.csv", False)
+        # # df = self.get_data("US100Cash_M30_2020_01_01_2020_12_31.csv", False)
+        # df = df[[self.AO, "close", "SMA_500", "RSI_14"]]
+        # train_data = self.scaler.transform(df)
+        # prediction = model.predict(train_data)
+        # df["result"] = prediction
+        # df.to_csv("results.csv")
 
 
 Solver().calculate()
--- test.py
+++ test.py
@@ -1,15 +1,48 @@
 import numpy as np
 from sklearn.linear_model import LinearRegression
-X = np.array([[1, 1], [1, 2], [2, 2], [2, 3]])
-# y = 1 * x_0 + 2 * x_1 + 3
-y = np.dot(X, np.array([1, 2])) + 3
+from sklearn.metrics import mean_squared_error
+from sklearn.preprocessing import PolynomialFeatures
+import matplotlib.pyplot as plt
 
-print("X", X)
-print("-"*50)
-print("y", y)
-print("-"*50)
+# X_train = np.array([[1, 1], [1, 2], [2, 2], [2, 3]])
+# # y = 1 * x_0 + 2 * x_1 + 3
+# Y_train = np.dot(X_train, np.array([1, 2])) + 3
 
-reg = LinearRegression().fit(X, y)
-result = reg.predict(np.array([[1, 4]])) # (1 * 1) + (2 * 4) + 3 = 12
-print(np.array([[1, 4]]))
-print("result", result)
+
+X_train = np.arange(0, 30)
+Y_train = [
+    3, 4, 5, 7, 10, 8, 9, 10, 10, 23, 27, 44, 50,
+    63, 67, 60, 62, 70, 75, 88, 81, 87,
+    95, 100, 108, 135, 151, 160, 169, 179
+]
+features = PolynomialFeatures(degree=2, include_bias=False)
+poly_features = features.fit_transform(X_train.reshape(-1, 1))
+model = LinearRegression()
+model.fit(poly_features, Y_train)
+
+
+pred_train = model.predict(np.array(poly_features))  # (1 * 1) + (2 * 4) + 3 = 12
+print("Y_train", Y_train)
+print("prediction", pred_train)
+print(mean_squared_error(Y_train, pred_train))
+
+
+plt.figure()
+plt.title("test")
+plt.scatter(X_train, pred_train)
+plt.plot(X_train, pred_train, c="red")
+plt.show()
+
+# print("-" * 50)
+
+# x_0 = -6400000.5415
+# x_1 = 1.651651
+# test = [x_0, x_1]
+# x_test = np.array([test])
+# y_test = np.array([
+#     (1 * x_0 + 2 * x_1 + 3)
+# ])
+# pred_test = model.predict(np.array([test]))
+# print("y_test", y_test)
+# print("prediction", pred_test)
+# print(mean_squared_error(y_test, pred_test))
